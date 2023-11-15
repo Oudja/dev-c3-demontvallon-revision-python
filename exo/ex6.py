f982a18c66ffd6b7fae0b86b2c192312112657fa
Oudja de Montvallon <demontvalon.oudja@gmail.com>
2023-11-15 16:51:11 +0100

    Exercice 6

---
+++ exo/ex6.py
@@ -0,0 +1,9 @@
+# Modules : Utilisez le module datetime pour afficher la date et l'heure actuelles.
+
+from datetime import datetime
+
+heure_actuelle = datetime.now()
+
+print("Date et heure actuelles :", heure_actuelle)
+
+
